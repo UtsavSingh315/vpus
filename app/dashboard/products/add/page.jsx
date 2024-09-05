cc992f5e6f11afa65a163bde54cf4baeaf7a3780
UtsavSingh315 <utsavsssingh@gmail.com>
2024-09-05 12:57:54 +0530

    ui

--- app/dashboard/products/add/page.jsx
+++ app/dashboard/products/add/page.jsx
@@ -7,7 +7,7 @@ const AddProductPage = () => {
       <form action={addProduct} className={styles.form}>
         <input type="text" placeholder="Title" name="title" required />
         <input type="text" placeholder="Department" name="Department" required />
-        <input type="text" placeholder="Price" name="price" required />
+        <input type="number" placeholder="Price" name="price" required />
         <input type="number" placeholder="Units" name="Units" required />
         <button type="submit">Submit</button>
       </form>
